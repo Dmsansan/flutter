5194c86a3b0a97f41f041ad5fee5606f5768fd6b
Sansan.si <sisansan123>
2021-09-08 13:49:51 +0800

    :rocket:支持调用后端接口代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,69 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());
+import 'package:http/http.dart' as http;
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
-    return new MaterialApp(
-      // title: 'Welcome to Flutter',
-      // home: new Scaffold(
-      //   appBar: new AppBar(
-      //     title: new Text('Welcome to Flutter'),
-      //   ),
-      //   body: new Center(
-      //     // child: new Text('Hello World'),
-      //     // child: new Text(wordPair.asPascalCase),
-      //     child: new RandomWords(),
-      //   ),
-      // ),
-      title: "StartUp Name Generator",
-      home: new RandomWords(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: MyHomePage(
+          // title: 'Flutter Demo',
+          ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  // MyHomePage({Key key, this.title}) : super(key: key);
+  // final String title;
+
   @override
-  createState() => new RandomWordsState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test Demo"),
       ),
-      body: _buildSuggestions(),
+      body: Center(
+        child: RaisedButton(
+          child: Text('测试数据接口'),
+          onPressed: () async {
+            String url = 'http://192.168.10.21:8081/test/first';
+            var res = await http.get(url);
+            String body = res.body;
+            //解析json数据
+            var json = jsonDecode(body);
+            print(json);
+
+            // 前端encode -> 后端decode
+            // 前端decode -> 后端encode
+          },
+        ),
+      ),
     );
   }
+}
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Hello',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.red),
       ),
     );
   }
